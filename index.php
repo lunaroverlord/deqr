2b5eaea36c20680d0932446439f4d620929aa0ec
olafs <olafs@222-98.nus.edu.sg>
2015-04-11 16:09:28 +0100

    Basic frontend

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+<title>Queuer - forget about standing in line</title>
+<link rel="stylesheet" type="text/css" href="qr.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="qr.js"></script>
+</head>
+
+<body>
+<button id="create">Create a queue</button>
+<button id="join">Join a queue</button>
+</body>
+
+</html>
